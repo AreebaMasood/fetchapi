16673dd7de2561227e88177ed7a14d40e797886d
Areeba Masood <areeba.enterprise123@gmail.com>
2022-05-26 11:57:53 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { ServiceService } from './service.service';
+import{HttpClient, HttpClientModule} from '@angular/common/http'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+  data:any;
+  
+  constructor(private user:ServiceService , private http :HttpClient){
+    // this.user.getdata().subscribe(data=>{
+    //   console.warn(data)
+    //   this.data=data
+    // })
+  }
+  getUserFormData(data:any)
+  {
+    // console.warn(data);
+    this.user.saveusers (data).subscribe((result)=>{
+    console.warn(result)
+    })
+  }
 }
